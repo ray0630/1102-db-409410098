c14a3bf557c68642302db586125c44b1620e3803
ray0630 <ray20020630@gmail.com>
2022-06-18 18:50:46 +0800

    w13-p1: create controllers for crown2 for MVC architecture

---
+++ controllers/crown2Controller_98.js
@@ -0,0 +1,36 @@
+
+const Category_98 = require('../models/Category_98');
+const Shop_98 = require('../models/Shop_98');
+
+exports.getCategories = async (req, res)=>{
+    try{
+        let results = await Category_98.fetchAll();
+        console.log('results',JSON.stringify(results));
+        res.render('crown2_98/index', { 
+          data: results,  
+          id: '409410098', 
+          title: "Crown2_DB"
+        });
+    }catch(err){
+        console.log(err);
+    }
+}
+
+exports.getProductByCategory = async (req, res)=>{
+    console.log('category',req.params.category);
+    try{
+        const cid = await Category_98.fetchCatidByName(req.params.category);
+        // console.log("cid",cid);
+        // const results = Shop_98.fetchProductByCategory(cid);
+        let results = await Shop_98.fetchProductByCategory(cid);
+        console.log("results",JSON.stringify(results));
+        res.render('crown2_98/product', { 
+        data: results,  
+        name: "林品睿",
+        id: '409410098', 
+        title: req.params.category
+        });
+    }catch(err){
+        console.log(err);
+    }
+}
--- routes/crown2_98.js
+++ routes/crown2_98.js
@@ -1,42 +1,16 @@
 var express = require('express');
 const { route } = require('express/lib/application');
+const req = require('express/lib/request');
 var router = express.Router();
 
 const Category_98 = require('../models/Category_98');
 const Shop_98 = require('../models/Shop_98');
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
-  try{
-    let results = await Category_98.fetchAll();
-    console.log('results',JSON.stringify(results));
-    res.render('crown2_98/index', { 
-      data: results,  
-      id: '409410098', 
-      title: "林品睿"
-    });
-  }catch(err){
-    console.log(err);
-  }
-});
+const crown2controller_98 = require('../controllers/crown2Controller_98')
 
-router.get('/shop_98/:category', async function(req,res){
-  console.log('category',req.params.category);
-  try{
-    const cid = await Category_98.fetchCatidByName(req.params.category);
-    // console.log("cid",cid);
-    // const results = Shop_98.fetchProductByCategory(cid);
-    let results = await Shop_98.fetchProductByCategory(cid);
-    console.log("results",JSON.stringify(results));
-    res.render('crown2_98/product', { 
-      data: results,  
-      name: "林品睿",
-      id: '409410098', 
-      title: req.params.category
-    });
-  }catch(err){
-    console.log(err);
-  }
-});
+/* GET home page. */
+router.get('/', crown2controller_98.getCategories);
+
+router.get('/shop_98/:category', crown2controller_98.getProductByCategory);
 
 module.exports = router;
