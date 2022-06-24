9c6df69ae6dc5bc4d8011d6a1433d751fcc8d91b
ray0630 <ray20020630@gmail.com>
2022-06-24 17:09:51 +0800

    w16-p3: /book_xx/create to book_xx/add_xx.ejs

--- routes/book_98.js
+++ routes/book_98.js
@@ -8,7 +8,7 @@ const db = require('../utils/database');
 router.get('/', async (req, res, next) => {
     try{
         const results = await db.query('SELECT * FROM book_98');
-        console.log('results',JSON.stringify(results.rows));
+        // console.log('results',JSON.stringify(results.rows));
         res.render('book_98/index', { 
             data: results.rows,
             id: '409410098',
@@ -17,6 +17,14 @@ router.get('/', async (req, res, next) => {
     }catch(err){
         console.log('err',error);
     }
+});
+
+// creat
+router.get('/create', async (req, res) => {
+    res.render('book_98/add_98', { 
+        id: '409410098',
+        name: '林品睿' 
+    });
 });
 
 module.exports = router;
