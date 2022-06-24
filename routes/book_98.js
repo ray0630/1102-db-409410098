90b0d8b4ae8ac50b1c359e46424f0ed24f3353fb
ray0630 <ray20020630@gmail.com>
2022-06-25 03:48:32 +0800

    w16-p4: POST /book_xx/create to create a book data

--- routes/book_98.js
+++ routes/book_98.js
@@ -4,11 +4,32 @@ var router = express.Router();
 
 const db = require('../utils/database');
 
-/* GET home page. */
+// CREAT
+router.post('/create', async (req, res) => {
+    console.log('body',req.body);
+    const id = req.body.id;
+    const name = req.body.name;
+    const author = req.body.author;
+    const price = req.body.price;
+
+    try{
+        const query = {
+            text: `INSERT INTO book_98 (id, name, author, price) 
+            VALUES ($1,$2,$3,$4)`,
+            values: [id, name, author, price]
+        };
+        await db.query(query);
+        res.redirect('/book_98');
+    }catch(err){
+        console.log(error);
+    }
+});
+
+/* READ */
 router.get('/', async (req, res, next) => {
     try{
         const results = await db.query('SELECT * FROM book_98');
-        // console.log('results',JSON.stringify(results.rows));
+        console.log('results',JSON.stringify(results.rows));
         res.render('book_98/index', { 
             data: results.rows,
             id: '409410098',
@@ -19,12 +40,16 @@ router.get('/', async (req, res, next) => {
     }
 });
 
-// creat
 router.get('/create', async (req, res) => {
     res.render('book_98/add_98', { 
         id: '409410098',
         name: '林品睿' 
     });
+});
+
+// DELETE
+router.get('/delete', async (req, res) => {
+
 });
 
 module.exports = router;
